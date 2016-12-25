2329dc9b77eca0da76cb61dbb5c0e004ffd9347c
Ricky <ricky.whitaker@undeadcodersociety.com>
2016-12-24 18:29:08 -0600

    removed extras, resized image

--- application/static/js/forms.js
+++
@@ -1,6 +0,0 @@
-$(document).ready( function () {
-    $('#profile_picture').change(function () {
-        var filename = $('#profile_picture').val().match(/[-_\w]+[.][\w]+$/i)[0];
-        $('#upload-text').text(filename);
-    });
-});
